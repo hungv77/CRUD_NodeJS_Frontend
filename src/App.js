f1151b4cf5743246cf00fbe40ccf7a24a553f526
hungv77 <kogome6a@gmail.com>
2024-03-15 01:24:59 +0700

    #5 Commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import Nav from "./components/Navigation/Nav";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Register from "./components/Register/Register";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import Users from "./components/ManageUsers/Users";
+import { useEffect, useState } from "react";
+import _ from "lodash";
 
 function App() {
+  const [account, setAccount] = useState({});
+
+  useEffect(() => {
+    let session = sessionStorage.getItem("account");
+    if (session) {
+      setAccount(JSON.parse(session));
+    }
+  }, []);
+
   return (
     <Router>
       <div className="app-container">
-        <Nav />
+        {account && !_.isEmpty(account) && account.isAuthenticated && <Nav />}
+
         <Switch>
           <Route path="/news">News</Route>
           <Route path="/about">About</Route>
@@ -19,10 +32,13 @@ function App() {
             Home
           </Route>
           <Route exact path="/login">
-            <Login/>
+            <Login />
           </Route>
           <Route exact path="/register">
-            <Register/>
+            <Register />
+          </Route>
+          <Route exact path="/users">
+            <Users />
           </Route>
           <Route path="*">404 not found</Route>
         </Switch>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -36,7 +36,22 @@ const Login = (props) => {
       return;
     }
 
-    await loginUser(valueLogin, password);
+    let response = await loginUser(valueLogin, password);
+
+    if(response && response.data && +response.data.EC === 0){
+      //success
+      let data = {
+        isAuthenticated: true,
+        token: 'fake token'
+      }
+      sessionStorage.setItem('account', JSON.stringify(data));
+      history.push('/users');
+    }
+
+    if(response && response.data && +response.data.EC !== 0){
+      //error
+      toast.error(response.data.EM)
+    }
   };
 
   return (
