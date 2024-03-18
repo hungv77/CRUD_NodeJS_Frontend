2b7f87dc040061084e65e85d7635714ff766b8c0
hungv77 <kogome6a@gmail.com>
2024-03-19 01:31:09 +0700

    #11 Pagination

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -1,9 +1,111 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import "./Users.scss";
+import { fetchAllUser } from "../../services/userService";
+import ReactPaginate from 'react-paginate';
 
 const Users = (props) => {
- 
-  return <div>user component</div>;
+  const [listUsers, setListUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentLimit, setCurrentLimit] = useState(3);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [currentPage]);
+
+  const fetchUsers = async () => {
+    let response = await fetchAllUser(currentPage, currentLimit);
+    if (response && response.data && response.data.EC === 0) {
+      setTotalPages(response.data.DT.totalPages);
+      setListUsers(response.data.DT.users);
+
+    }
+  };
+
+  const handlePageClick = async (event) => {
+    setCurrentPage(+event.selected + 1);
+  };
+
+  return (
+    <div className="container">
+      <div className="manage-users-container">
+        <div className="user-header">
+          <div className="title">
+            <h3>Table Users</h3>
+          </div>
+          <div className="actions">
+            <button className="btn btn-success">Refresh</button>
+            <button className="btn btn-primary">Add New User</button>
+          </div>
+        </div>
+        <div className="user-body">
+          <table className="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">ID</th>
+                <th scope="col">Email</th>
+                <th scope="col">Username</th>
+                <th scope="col">Group</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {listUsers && listUsers.length > 0 ? (
+                <>
+                  {listUsers.map((item, index) => {
+                    return (
+                      <tr key={`row-${index}`}>
+                        <td>{index + 1}</td>
+                        <td>{item.id}</td>
+                        <td>{item.email}</td>
+                        <td>{item.username}</td>
+                        <td>{item.Group ? item.Group.name : ""}</td>
+                        <td>
+                          <button className="btn btn-warning">Edit</button>
+                          <button className="btn btn-danger">Delete</button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </>
+              ) : (
+                <>
+                  <tr><td>I have nothing, nothing, nothing !!!</td></tr>
+                </>
+              )}
+            </tbody>
+          </table>
+        </div>
+
+        {totalPages > 0 &&
+        <div className="user-footer">
+        <ReactPaginate
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={4}
+          pageCount={totalPages}
+          previousLabel="< previous"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+          renderOnZeroPageCount={null}
+        />
+        </div>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Users;
